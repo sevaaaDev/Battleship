aa3fbb47e9a866ee521142a5e260ee866715b77a
sevaaa <136947471+sevaaaDev@users.noreply.github.com>
2023-12-04 13:39:53 +0700

    done test ship

--- src/ship.test.js
+++ src/ship.test.js
@@ -4,7 +4,24 @@ test("See ship info", () => {
   const ship = new Ship(4);
   expect(ship).toEqual({
     length: 4,
-    hit: 0,
+    numOfHit: 0,
     sunk: false,
   });
+});
+
+test("hitting the ship", () => {
+  const ship = new Ship(4);
+  ship.hit();
+  expect(ship.numOfHit).toBe(1);
+});
+
+test("the ship sunk", () => {
+  const ship = new Ship(4);
+  ship.hit();
+  ship.hit();
+  ship.hit();
+  ship.hit();
+  ship.isSunk();
+  expect(ship.sunk).toBe(true);
+  expect(ship.isSunk()).toBe(true);
 });
