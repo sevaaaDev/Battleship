01367153beab80e48f4ba9bddb1768197e5698c1
sevaaa <136947471+sevaaaDev@users.noreply.github.com>
2023-12-05 12:50:49 +0700

    import Ship module

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,23 +1,29 @@
+import Ship from "./ship";
 export default class Gameboard {
   constructor() {
     this.missedAttack = [];
     this.attack = [];
     this.numOfShip = 0;
     this.board = this.#createBoard();
+    this.ships = [];
   }
 
   placeShip(length, orientation, x, y) {
+    let ship = new Ship(length);
+    this.ships.push(ship);
     if (orientation === "x") {
       for (let i = x; i < length; i++) {
-        this.board[i][y] = "ship";
+        this.board[i][y] = ship;
       }
     }
     this.numOfShip++;
   }
 
   receiveAttack(x, y) {
-    if (this.board[x][y] === "ship") {
+    if (this.board[x][y].length !== 0) {
+      this.board[x][y].hit();
       this.board[x][y] = "hit";
+      this.attack.push({ x, y });
       return true;
     }
     this.missedAttack.push({ x, y });
@@ -25,11 +31,9 @@ export default class Gameboard {
   }
 
   isAllSunk() {
-    for (let i = 0; i < 10; i++) {
-      for (let j = 0; j < 10; j++) {
-        if (this.board[j][i] === "ship") {
-          return false;
-        }
+    for (let ship of this.ships) {
+      if (!ship.isSunk()) {
+        return false;
       }
     }
     return true;
