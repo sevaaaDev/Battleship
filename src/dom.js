a82e4ecb750d1bf54e923a61238208e331c5dcd2
sevaaa <136947471+sevaaaDev@users.noreply.github.com>
2023-12-12 12:27:37 +0700

    dom module

---
+++ src/dom.js
@@ -0,0 +1,32 @@
+export function createDom(playerData, compData) {
+  const playerBoard = document.querySelector(".playerBoard .board");
+  const compBoard = document.querySelector(".compBoard .board");
+  populateGrid(playerBoard, playerData);
+  populateGrid(compBoard, compData);
+}
+
+function populateGrid(board, data) {
+  resetGrid(board);
+  for (let y = 9; y >= 0; y--) {
+    for (let x = 0; x < 10; x++) {
+      const grid = document.createElement("div");
+      grid.setAttribute("data-x", x);
+      grid.setAttribute("data-y", y);
+      for (let ship of data.ships) {
+        if (ship.x === x && ship.y === y) {
+          grid.style.backgroundColor = "black";
+        }
+      }
+      for (let attack of data.missedAttack) {
+        if (attack.x === x && attack.y === y) {
+          grid.style.backgroundColor = "red";
+        }
+      }
+      board.append(grid);
+    }
+  }
+}
+
+function resetGrid(board) {
+  board.innerHTML = "";
+}
