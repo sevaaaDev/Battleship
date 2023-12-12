f5278b3872ac58f6f4d0a113a9198cdab3b9be0b
sevaaa <136947471+sevaaaDev@users.noreply.github.com>
2023-12-12 12:27:30 +0700

    update dom when attack happen

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 import Gameboard from "./gameboard";
 import { attackRandom } from "./player";
+import { createDom } from "./dom";
 import "./style/style.css";
 
-export function game() {
+function game() {
   const playerGameboard = new Gameboard();
   const computerGameboard = new Gameboard();
   playerGameboard.placeShip(1, "x", 0, 0);
   computerGameboard.placeShip(1, "x", 0, 0);
+  createDom(playerGameboard, computerGameboard);
   playRound();
   function playRound() {
     let x = prompt("x");
     let y = prompt("y");
-    computerGameboard.receiveAttack(x, y);
+    computerGameboard.receiveAttack(+x, +y);
     attackRandom(playerGameboard);
+    createDom(playerGameboard, computerGameboard);
     if (isAllSunk(computerGameboard, playerGameboard)) {
       alert("end");
       return;
     }
-    playRound();
   }
 }
 
@@ -26,3 +28,5 @@ function isAllSunk(comp, player) {
     return true;
   }
 }
+
+game();
