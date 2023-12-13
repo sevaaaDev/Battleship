e2ebc5f84b434ae3739faafca63a3423a9761159
sevaaa <136947471+sevaaaDev@users.noreply.github.com>
2023-12-13 15:37:14 +0700

    add event listener to board

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import { createDom } from "./dom";
 import "./style/style.css";
 
 function game() {
-  const playerGameboard = new Gameboard();
   const computerGameboard = new Gameboard();
-  playerGameboard.placeShip(1, "x", 0, 0);
-  computerGameboard.placeShip(1, "x", 0, 0);
+  document.addEventListener("click", (e) => {
+    if (e.target.matches(".compBoard .board div")) {
+      const body = document.querySelector("body");
+      body.style.pointerEvents = "none";
+      playRound(e.target.dataset.x, e.target.dataset.y);
+      body.removeAttribute("style");
+    }
+  });
+  const playerGameboard = new Gameboard();
+  playerGameboard.placeShip(3, "y", 2, 9);
+  computerGameboard.placeShip(5, "x", 4, 2);
   createDom(playerGameboard, computerGameboard);
   playRound();
   function playRound() {
