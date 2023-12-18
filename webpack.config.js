6ab79e3b63aeaefd98f66ea292bc6d525022d8a7
sevaaa <136947471+sevaaaDev@users.noreply.github.com>
2023-12-18 12:15:30 +0700

    enable css module

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,15 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
       },
     ],
   },
