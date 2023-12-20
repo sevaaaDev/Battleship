812bb6ae81ad7e0c9fd2d9f61d69394d3caaebb0
sevaaa <136947471+sevaaaDev@users.noreply.github.com>
2023-12-20 14:38:54 +0700

    add modal

--- src/dom/game.js
+++ src/dom/game.js
@@ -1,36 +1,45 @@
 import styles from "./gameUi.css";
 
 export function displayGame() {
-  console.log(styles);
   const body = document.querySelector("body");
-  body.innerHTML = "";
-  const title = document.createElement("h1");
-  title.innerText = "Battleship";
-  title.classList.add(styles.title);
-  const info = document.createElement("p");
-  info.classList.add(styles.info);
-  const container = document.createElement("div");
-  container.classList.add(styles.container);
-  board("player", container);
-  const restart = document.createElement("button");
-  restart.setAttribute("type", "button");
-  restart.classList.add(styles.restartBtn);
-  restart.innerText = "restart";
-  container.append(restart);
-  board("comp", container);
-  body.append(title, info, container);
+  body.innerHTML = `
+<h1 class="${styles.title}">Battleship</h1>
+<p class="${styles.info}"></p>
+<div class="${styles.container}">
+  <div class="${styles.playerBoard}">
+    <h3 class="${styles.name}">You</h3>
+    <div class="${styles.board}"></div>
+  </div>
+  <button type='button' class='${styles.restartBtn}'>Restart</button>
+  <div class="${styles.compBoard}">
+    <h3 class="${styles.name}">Enemy</h3>
+    <div class="${styles.board}"></div>
+  </div>
+  <dialog class='${styles.modal}'>
+     <h2 class="${styles.title}">Place your Ship</h2>
+     <div class="${styles.container}">
+       <div class='${styles.board}'></div>
+     </div> 
+     <button type='button' class="${styles.button}" aria-disabled='true'>Start</button>
+  </dialog>
+</div>
+
+`;
+  // const title = document.createElement("h1");
+  // title.innerText = "Battleship";
+  // title.classList.add(styles.title);
+  // const info = document.createElement("p");
+  // info.classList.add(styles.info);
+  // const container = document.createElement("div");
+  // container.classList.add(styles.container);
+  // board("player", container);
+  // const restart = document.createElement("button");
+  // restart.setAttribute("type", "button");
+  // restart.classList.add(styles.restartBtn);
+  // restart.innerText = "restart";
+  // container.append(restart);
+  // board("comp", container);
+  // body.append(title, info, container);
 
   console.log(body);
-}
-
-function board(name, container) {
-  const userBoard = document.createElement("div");
-  const board = document.createElement("div");
-  const nameInfo = document.createElement("h3");
-  userBoard.classList.add(styles[`${name}Board`]);
-  nameInfo.innerText = name;
-  nameInfo.classList.add(styles.name);
-  board.classList.add(styles.board);
-  userBoard.append(nameInfo, board);
-  container.append(userBoard);
 }
