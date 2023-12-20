8f0d4be8e2735f23235d24b71f1c4c6522ac105f
sevaaa <136947471+sevaaaDev@users.noreply.github.com>
2023-12-20 14:39:11 +0700

    refactor

--- src/dom/placeShip.js
+++ src/dom/placeShip.js
@@ -1,21 +1,13 @@
-import styles from "./placeShipUi.css";
+import styles from "./gameUi.css";
 export function placeShipUi(data) {
-  const body = document.querySelector("body");
-  body.innerHTML = `
- <h2 class="${styles.title}">Place your Ship</h2>
- <div class="${styles.container}">
-   <div class='${styles.board}'></div>
-   <button type='button' class="${styles.button}" aria-disabled='true'>Start</button>
- </div> 
-`;
-  const board = document.querySelector(`.${styles.board}`);
+  const board = document.querySelector(`dialog .${styles.board}`);
+  board.innerHTML = "";
   for (let y = 9; y >= 0; y--) {
     for (let x = 0; x < 10; x++) {
       const grid = document.createElement("div");
       grid.setAttribute("data-x", x);
       grid.setAttribute("data-y", y);
       board.append(grid);
-      console.log(data);
       for (let ship of data.ships) {
         for (let coord of ship.range) {
           if (x === coord.x && y === coord.y) {
