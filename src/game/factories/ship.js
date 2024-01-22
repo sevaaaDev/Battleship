74f75028d7101d241684fe25b3ccabb148a45d5f
sevaaa <136947471+sevaaaDev@users.noreply.github.com>
2024-01-22 14:50:04 +0700

    change Ship class to factory

--- src/game/factories/ship.js
+++ src/game/factories/ship.js
@@ -1,20 +1,41 @@
-export default class Ship {
-  constructor(length, name) {
-    this.length = length;
-    this.health = length;
-    this.sunk = false;
-    this.name = name;
-  }
+// export default class Ship {
+//   constructor(length, name) {
+//     this.length = length;
+//     this.health = length;
+//     this.sunk = false;
+//     this.name = name;
+//   }
+//
+//   hit() {
+//     if (this.health) this.health--;
+//   }
+//
+//   isSunk() {
+//     if (!this.health) {
+//       this.sunk = true;
+//       return true;
+//     }
+//     return false;
+//   }
+// }
 
-  hit() {
-    if (this.health) this.health--;
-  }
-
-  isSunk() {
-    if (!this.health) {
-      this.sunk = true;
-      return true;
-    }
-    return false;
-  }
+export default function createShip(length, name) {
+  const proto = {
+    isSunk: function () {
+      if (!this.health) {
+        this.sunk = true;
+        return true;
+      }
+      return false;
+    },
+    hit: function () {
+      if (this.health) this.health--;
+    },
+  };
+  const obj = Object.create(proto);
+  obj.length = length;
+  obj.name = name;
+  obj.health = length;
+  obj.sunk = false;
+  return obj;
 }
