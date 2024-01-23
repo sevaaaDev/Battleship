703db7dcfd60932815ae6d84f13091c359bc98e9
sevaaa <136947471+sevaaaDev@users.noreply.github.com>
2024-01-23 12:17:58 +0700

    add pos and orient prop to ship obj

--- src/game/factories/ship.js
+++ src/game/factories/ship.js
@@ -10,11 +10,17 @@ const proto = {
     if (this.health) this.health--;
   },
 };
-export default function createShip(length, name) {
+export default function createShip(length, name, pos, orient) {
   const obj = Object.create(proto);
   obj.length = length;
   obj.name = name;
   obj.health = length;
   obj.sunk = false;
+  obj.head = pos;
+  obj.orientation = orient;
+  obj.tail = [pos[0], pos[1] + length];
+  if (orient === "x") {
+    obj.tail = [pos[0] + length, pos[1]];
+  }
   return obj;
 }
