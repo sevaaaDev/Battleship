979f17f64183c86e4efb030fad1b2eebb067fcdd
sevaaa <136947471+sevaaaDev@users.noreply.github.com>
2024-01-31 15:55:23 +0700

    test

--- src/test/gameboard.test.js
+++ src/test/gameboard.test.js
@@ -21,11 +21,10 @@ test("there are 2 ship", () => {
 
 test("cant place ship at the same place", () => {
   const board = createGameboard();
-  // fix this
-  expect(board.placeShip(4, "x", 2, 0, "Destroyer")).toBe(1);
-  board.placeShip(5, "x", 0, 0, "Carrier");
+  board.placeShip(4, "x", 2, 4, "Destroyer");
+  board.placeShip(5, "x", 0, 4, "Carrier");
+  board.placeShip(4, "y", 3, 3, "Patrol");
   expect(board.ships.length).toBe(1);
-  expect(board.ships[0].name).toBe("Destroyer");
 });
 
 test("cant place a ship outside of the board", () => {
@@ -33,4 +32,41 @@ test("cant place a ship outside of the board", () => {
   board.placeShip(4, "x", 9, 0, "Destroyer");
   board.placeShip(4, "y", 0, 8, "Destroyer");
   expect(board.ships.length).toBe(0);
+});
+
+test("attack ship", () => {
+  const board = createGameboard();
+  board.placeShip(4, "x", 0, 0, "Destroyer");
+  expect(board.receiveAttack(1, 0)).toBe("hit");
+  expect(board.ships[0].health).toBe(3);
+  expect(board.board[1][0]).toBe("hit");
+});
+
+test("cant attack the same place twice", () => {
+  const board = createGameboard();
+  board.placeShip(4, "x", 0, 0, "Destroyer");
+  board.receiveAttack(1, 0);
+  expect(board.receiveAttack(1, 0)).toBe(false);
+});
+
+test("cant attack outside the board", () => {
+  const board = createGameboard();
+  board.placeShip(4, "x", 0, 0, "Destroyer");
+  expect(board.receiveAttack(10, 0)).toBe(false);
+});
+
+test("attack water", () => {
+  const board = createGameboard();
+  board.placeShip(4, "x", 0, 0, "Destroyer");
+  expect(board.receiveAttack(7, 0)).toBe("missed");
+});
+test("are all sunk?", () => {
+  const board = createGameboard();
+  board.placeShip(4, "x", 0, 0, "Destroyer");
+  board.receiveAttack(0, 0);
+  board.receiveAttack(1, 0);
+  board.receiveAttack(2, 0);
+  expect(board.areAllSunk()).toBe(false);
+  board.receiveAttack(3, 0);
+  expect(board.areAllSunk()).toBe(true);
 });
