acf123b0b5ab758d3970ce00d196f721a2f94e49
sevaaa <136947471+sevaaaDev@users.noreply.github.com>
2024-02-01 14:56:31 +0700

    wip

--- src/factories/player.js
+++ src/factories/player.js
@@ -1,1 +1,20 @@
+import createGameboard from "./gameboard";
+
+export default function createPlayer() {
+  return {
+    board: createGameboard(),
+    attack(board, x, y) {
+      if (toString(x) && toString(y)) {
+        return board.receiveAttack(x, y);
+      }
+      const result = board.receiveAttack(
+        Math.floor(Math.random() * 9),
+        Math.floor(Math.random() * 9),
+      );
+      if (!result) {
+        return this.attack(board);
+      }
+      return result;
+    },
+  };
 }
