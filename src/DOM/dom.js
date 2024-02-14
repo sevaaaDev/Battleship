0ada38177baad9ca8303f6e06f8aaa24f2706390
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-02-14 13:00:16 +0700

    create domHit and domMissed func to paint the tile after attack

--- src/DOM/dom.js
+++ src/DOM/dom.js
@@ -24,4 +24,16 @@ function renderBoard(board) {
   }
 }
 
-export { render, renderBoard };
+function domHit(x, y) {
+  let tile = document.querySelector(`div[data-x='${x}'][data-y='${y}']`);
+  tile.classList.remove(`${css.ship}`);
+  tile.classList.add(`${css.hit}`);
+}
+
+function domMiss(x, y) {
+  let tile = document.querySelector(`div[data-x='${x}'][data-y='${y}']`);
+  tile.classList.remove(`${css.disabled}`);
+  tile.classList.add(`${css.miss}`);
+}
+
+export { render, renderBoard, domHit, domMiss };
