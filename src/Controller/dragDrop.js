7361508065d5d37d94dc0be534fd7ee5ef29fd7d
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-03-05 16:55:36 +0700

    implement dragging effect

--- src/Controller/dragDrop.js
+++ src/Controller/dragDrop.js
@@ -1,3 +1,4 @@
+import css from "../style.css";
 export function startDrag(e, gameboard, renderBoard) {
   let currentElement = e.target;
   let isDragged = false;
@@ -6,7 +7,9 @@ export function startDrag(e, gameboard, renderBoard) {
 
   function draggingMobile(e) {
     e.preventDefault();
+    console.log(e.target);
     isDragged = true;
+    animateDrag(e, currentElement, gameboard);
   }
   function endDrag(e) {
     document.removeEventListener("pointerover", draggingMobile);
@@ -19,6 +22,70 @@ export function startDrag(e, gameboard, renderBoard) {
   }
   document.addEventListener("pointerover", draggingMobile, { passive: false });
   document.addEventListener("pointerup", endDrag, { once: true });
+}
+
+function animateDrag(e, currentElement, gameboard) {
+  let [currentX, currentY] = [+e.target.dataset.x, +e.target.dataset.y];
+  let [previousX, previousY] = [
+    +e.relatedTarget.dataset.x,
+    +e.relatedTarget.dataset.y,
+  ];
+  let { x, y } = currentElement.dataset;
+  let ship = gameboard.board[+x][+y];
+  let length = parseInt(ship.length);
+  let name = ship.name;
+  let orientation = ship.orientation;
+  // TODO: refactor this mess
+  if (!isNaN(previousX)) {
+    if (!gameboard.isOutside(previousX, previousY, length, orientation)) {
+      resetTileColor(previousX, previousY, length, orientation);
+    }
+  }
+  if (!e.target.dataset.drop) return;
+  // TODO: create checkWater() function
+  if (gameboard.isOutside(currentX, currentY, length, orientation)) return;
+  if (gameboard.isThereAShip(currentX, currentY, length, orientation, name))
+    return;
+  if (
+    gameboard.isTooCloseToOtherShip(
+      currentX,
+      currentY,
+      length,
+      orientation,
+      name,
+    )
+  )
+    return;
+
+  changeTileColor(currentX, currentY, length, orientation);
+}
+
+function changeTileColor(x, y, length, orientation) {
+  for (let i = 0; i < length; i++) {
+    let shipNode = document.querySelector(
+      `div[data-board="player"] div[data-x="${x}"][data-y="${y}"]`,
+    );
+    shipNode.classList.add(`${css.drag}`);
+    if (orientation === "x") {
+      x++;
+      continue;
+    }
+    y++;
+  }
+}
+
+function resetTileColor(x, y, length, orientation) {
+  for (let i = 0; i < length; i++) {
+    let shipNode = document.querySelector(
+      `div[data-board="player"] div[data-x="${x}"][data-y="${y}"]`,
+    );
+    shipNode.classList.remove(`${css.drag}`);
+    if (orientation === "x") {
+      x++;
+      continue;
+    }
+    y++;
+  }
 }
 
 function drop(currentElement, dropPoint, gameboard) {
