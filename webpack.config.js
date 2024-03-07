d75f90af3ddcd7b8b24f7cd792bedf187d692954
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-03-07 16:05:24 +0700

    change config

--- webpack.config.js
+++ webpack.config.js
@@ -33,16 +33,8 @@ module.exports = {
         ],
       },
       {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "fonts/",
-            },
-          },
-        ],
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
       },
     ],
   },
