8f4b31e2f236d9c58420ffc45310285db3029086
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2024-03-20 15:26:55 +0700

    webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
 const path = require("path");
 const htmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+process.env.NODE_ENV = "production";
 const devMode = process.env.NODE_ENV !== "production";
 
 module.exports = {
-  mode: "development",
+  mode: devMode,
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
-  devtool: "inline-source-map",
+  // devtool: "inline-source-map",
   devServer: {
     watchFiles: ["dist/**/*"],
   },
@@ -19,8 +20,7 @@ module.exports = {
       filename: "index.html",
       template: "src/view/template.html",
     }),
-    new MiniCssExtractPlugin(),
-  ],
+  ].concat(devMode ? [] : [new MiniCssExtractPlugin()]),
   module: {
     rules: [
       {
